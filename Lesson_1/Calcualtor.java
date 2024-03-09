5b786dabf568bc649f669f3f1f9e770e4b443e7d
PampersKrut <128176492+PampersKrut@users.noreply.github.com>
2024-03-09 18:18:02 +0200

    Add files via upload

---
+++ Lesson_1/Calcualtor.java
@@ -0,0 +1,7 @@
+package Lesson_1;
+
+public class Calcualtor {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ Lesson_1/CyclesTheme.java
@@ -0,0 +1,23 @@
+package Lesson_1;
+
+public class CyclesTheme {
+    public static void main(String[] args) {
+        System.out.println("1. Подсчет суммы четных и нечетных чисел\n");
+        int minNum = -10;
+        int maxNum = 21;
+        int count = minNum;
+        int sumEven = 0;
+        int sumOdd = 0;
+
+        do {
+            if (count % 2 == 0) {
+                sumEven += count;
+                System.out.println(sumEven + "   " +count);
+            } else if (Math.abs(count) % 2!= 0) {
+                sumOdd += count;
+            }
+            count++;
+        } while (count <= maxNum);
+        System.out.println("Четные числа: " + sumEven + "\nНечетные числа: " + sumOdd);
+    }
+}
